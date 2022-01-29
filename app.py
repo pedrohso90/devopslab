7421aadbc6f5e206c6b3dbfcc8d2019938865523
Pedro Oliveira <pedoliveira@uolinc.com>
2022-01-29 09:46:37 -0300

    first commit - add app python and pipeline github actions

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_inicial():
+    return "Hello World"
+
+@app.route("/soma")
+def soma_valores():
+    x=10+10
+    return "Sua soma de 10+10="+str(x)
+
+if __name__ == '__main__':
+    app.run(debug=True)
