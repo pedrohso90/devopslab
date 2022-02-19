d02b7365d47990e1237d126bbf20f4f8cf94d25b
Pedro Oliveira <pedrohso90@gmail.com>
2022-02-19 08:01:46 -0300

    change table name

--- db/user.sql
+++ db/user.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [IF NOT EXISTS] customers (
+CREATE TABLE IF NOT EXISTS customers (
    user_id serial PRIMARY KEY,
    username VARCHAR ( 50 ) UNIQUE NOT NULL,
    email VARCHAR ( 255 ) UNIQUE NOT NULL,
